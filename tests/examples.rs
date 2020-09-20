c031675ec3bb316d3bb6f137ff796f8b3c39ebc2
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-09-20 12:03:52 +0200

    Third test not passing

--- tests/examples.rs
+++ tests/examples.rs
@@ -70,6 +70,39 @@ fn avoiding_conflict_during_decision_making() {
     assert_eq!(solution, solver_solution);
 }
 
+#[test]
+/// https://github.com/dart-lang/pub/blob/master/doc/solver.md#performing-conflict-resolution
+fn conflict_resolution() {
+    let mut solver = SimpleCache::new();
+    #[rustfmt::skip]
+    solver.add_dependencies(
+        "root", version(1, 0, 0),
+        vec![("foo", Range::higher_than(version(1, 0, 0)))].into_iter(),
+    );
+    #[rustfmt::skip]
+    solver.add_dependencies(
+        "foo", version(2, 0, 0),
+        vec![("bar", Range::between(version(1, 0, 0), version(2, 0, 0)))].into_iter(),
+    );
+    solver.add_dependencies("foo", version(1, 0, 0), vec![].into_iter());
+    #[rustfmt::skip]
+    solver.add_dependencies(
+        "bar", version(1, 0, 0),
+        vec![("foo", Range::between(version(1, 0, 0), version(2, 0, 0)))].into_iter(),
+    );
+
+    // Run the solver.
+    let solver_solution = solver.run(&"root", &version(1, 0, 0)).unwrap();
+
+    // Solution.
+    let mut solution = HashMap::new();
+    solution.insert("root", version(1, 0, 0));
+    solution.insert("foo", version(1, 0, 0));
+
+    // Comparing the true solution with the one computed by the solver.
+    assert_eq!(solution, solver_solution);
+}
+
 /// helper functions to create versions.
 fn version(major: usize, minor: usize, patch: usize) -> SemanticVersion {
     SemanticVersion::new(major, minor, patch)
