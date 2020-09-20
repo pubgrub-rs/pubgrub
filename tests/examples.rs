08a65f2d50979fe335af45314dfdfff9c6e5696b
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-09-20 11:58:28 +0200

    Passing second test

--- tests/examples.rs
+++ tests/examples.rs
@@ -6,6 +6,7 @@ use pubgrub::solver::Solver;
 use pubgrub::version::SemanticVersion;
 
 #[test]
+/// https://github.com/dart-lang/pub/blob/master/doc/solver.md#no-conflicts
 fn no_conflict() {
     let mut solver = SimpleCache::new();
     #[rustfmt::skip]
@@ -29,6 +30,41 @@ fn no_conflict() {
     solution.insert("root", version(1, 0, 0));
     solution.insert("foo", version(1, 0, 0));
     solution.insert("bar", version(1, 0, 0));
+
+    // Comparing the true solution with the one computed by the solver.
+    assert_eq!(solution, solver_solution);
+}
+
+#[test]
+/// https://github.com/dart-lang/pub/blob/master/doc/solver.md#avoiding-conflict-during-decision-making
+fn avoiding_conflict_during_decision_making() {
+    let mut solver = SimpleCache::new();
+    #[rustfmt::skip]
+    solver.add_dependencies(
+        "root", version(1, 0, 0),
+        vec![
+            ("foo", Range::between(version(1, 0, 0), version(2, 0, 0))),
+            ("bar", Range::between(version(1, 0, 0), version(2, 0, 0))),
+        ].into_iter(),
+    );
+    #[rustfmt::skip]
+    solver.add_dependencies(
+        "foo", version(1, 1, 0),
+        vec![("bar", Range::between(version(2, 0, 0), version(3, 0, 0)))].into_iter(),
+    );
+    solver.add_dependencies("foo", version(1, 0, 0), vec![].into_iter());
+    solver.add_dependencies("bar", version(1, 0, 0), vec![].into_iter());
+    solver.add_dependencies("bar", version(1, 1, 0), vec![].into_iter());
+    solver.add_dependencies("bar", version(2, 0, 0), vec![].into_iter());
+
+    // Run the solver.
+    let solver_solution = solver.run(&"root", &version(1, 0, 0)).unwrap();
+
+    // Solution.
+    let mut solution = HashMap::new();
+    solution.insert("root", version(1, 0, 0));
+    solution.insert("foo", version(1, 0, 0));
+    solution.insert("bar", version(1, 1, 0));
 
     // Comparing the true solution with the one computed by the solver.
     assert_eq!(solution, solver_solution);
