055fbecd250a6f1b10a0d95f8c1a4efed342545c
Jacob Finkelman <YeomanYaacov@gmail.com>
2020-10-16 05:03:23 -0400

    test: port property tests from Cargo (#28)
    * test: add a mostly direct proptest port from Cargo
    
    * style: clean up
    
    * test: return OfflineDependencyProvider directly
    
    * test: add a first prop test
    
    * test: use ints instead of Strings to avoid expensive clones
    
    * test: check that algorithm is deterministic
    
    * test: port over prop_minimum_version_errors_the_same
    
    * test: port prop_removing_a_dep_cant_break
    
    * test: port prop_limited_independence_of_irrelevant_alternatives
    
    * test: port meta_test_deep_trees_from_strategy
    
    * ci: don't shrink on ci
    
    * feat: add a timeout
    
    * style: apply review suggestions
    
    * style: change license header to SPDX
    
    * docs: link to DependencyProvider.callback
    
    * style: proofread naming and comments
    
    * style: rename ReversedDependencyProvider
    
    * rename `callback` to `should_cancel`
    
    Co-authored-by: Alex Tokarev <aleksator@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -19,7 +19,7 @@ pub enum PubGrubError<P: Package, V: Version> {
     /// [DependencyProvider](crate::solver::DependencyProvider)
     /// returned an error in the method
     /// [list_available_versions](crate::solver::DependencyProvider::list_available_versions).
-    #[error("Retrieving available versions of package {package} failed)")]
+    #[error("Retrieving available versions of package {package} failed")]
     ErrorRetrievingVersions {
         /// Package for which we want the list of versions.
         package: P,
@@ -32,7 +32,7 @@ pub enum PubGrubError<P: Package, V: Version> {
     /// [DependencyProvider](crate::solver::DependencyProvider)
     /// returned an error in the method
     /// [get_dependencies](crate::solver::DependencyProvider::get_dependencies).
-    #[error("Retrieving dependencies of {package} {version} failed)")]
+    #[error("Retrieving dependencies of {package} {version} failed")]
     ErrorRetrievingDependencies {
         /// Package whose dependencies we want.
         package: P,
@@ -42,6 +42,11 @@ pub enum PubGrubError<P: Package, V: Version> {
         /// [DependencyProvider](crate::solver::DependencyProvider).
         source: Box<dyn std::error::Error>,
     },
+
+    /// Error arising when the implementer of [DependencyProvider](crate::solver::DependencyProvider)
+    /// returned an error in the method [should_cancel](crate::solver::DependencyProvider::should_cancel).
+    #[error("We should cancel")]
+    ErrorShouldCancel(Box<dyn std::error::Error>),
 
     /// Something unexpected happened.
     #[error("{0}")]
