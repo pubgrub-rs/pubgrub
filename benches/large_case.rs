25a1c8d63196d6ebbbf7cd0cf4a1eb291f5a94b1
Jacob Finkelman <YeomanYaacov@gmail.com>
2020-10-24 10:50:15 -0400

    test: switch to criterion (#47)

--- benches/large_case.rs
+++ benches/large_case.rs
@@ -1,22 +1,38 @@
 // SPDX-License-Identifier: MPL-2.0
+use std::time::Duration;
 
-#![feature(test)]
-extern crate test;
-use test::Bencher;
+extern crate criterion;
+use self::criterion::*;
 
-use pubgrub::solver::{resolve, OfflineDependencyProvider};
+use pubgrub::solver::{resolve, DependencyProvider, OfflineDependencyProvider};
 use pubgrub::version::NumberVersion;
 
-#[cfg(feature = "serde")]
-#[bench]
-/// This is an entirely synthetic benchmark. It may not be realistic.
-fn large_case(b: &mut Bencher) {
-    let s = std::fs::read_to_string("test-examples/large_case_u16_NumberVersion.ron").unwrap();
-    let dependency_provider: OfflineDependencyProvider<u16, NumberVersion> =
-        ron::de::from_str(&s).unwrap();
+fn bench_nested(c: &mut Criterion) {
+    let mut group = c.benchmark_group("large_cases");
+    group.measurement_time(Duration::from_secs(20));
 
-    // bench
-    b.iter(|| {
-        let _ = resolve(&dependency_provider, 0, 0);
-    });
+    for case in std::fs::read_dir("test-examples").unwrap() {
+        let case = case.unwrap().path();
+
+        group.bench_function(
+            format!("{}", case.file_name().unwrap().to_string_lossy()),
+            |b| {
+                let s = std::fs::read_to_string(&case).unwrap();
+                let dependency_provider: OfflineDependencyProvider<u16, NumberVersion> =
+                    ron::de::from_str(&s).unwrap();
+                let all_versions = dependency_provider.list_available_versions(&0).unwrap();
+
+                b.iter(|| {
+                    for &n in &all_versions {
+                        let _ = resolve(&dependency_provider, 0, n);
+                    }
+                });
+            },
+        );
+    }
+
+    group.finish();
 }
+
+criterion_group!(benches, bench_nested);
+criterion_main!(benches);
