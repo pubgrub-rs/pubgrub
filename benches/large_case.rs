2d7cac9a689bd37eee1298f09876254d96640e55
Eh2406 <YeomanYaacov@gmail.com>
2020-11-14 22:54:03 -0500

    test: use the elm benchmark

--- benches/large_case.rs
+++ benches/large_case.rs
@@ -5,7 +5,7 @@ extern crate criterion;
 use self::criterion::*;
 
 use pubgrub::package::Package;
-use pubgrub::solver::{resolve, DependencyProvider, OfflineDependencyProvider};
+use pubgrub::solver::{resolve, OfflineDependencyProvider};
 use pubgrub::version::{NumberVersion, SemanticVersion, Version};
 use serde::de::Deserialize;
 use std::hash::Hash;
@@ -13,14 +13,14 @@ use std::hash::Hash;
 fn bench<'a, P: Package + Deserialize<'a>, V: Version + Hash + Deserialize<'a>>(
     b: &mut Bencher,
     case: &'a str,
-    p: P,
 ) {
     let dependency_provider: OfflineDependencyProvider<P, V> = ron::de::from_str(&case).unwrap();
-    let all_versions = dependency_provider.list_available_versions(&p).unwrap();
 
     b.iter(|| {
-        for n in &all_versions {
-            let _ = resolve(&dependency_provider, p.clone(), n.clone());
+        for p in dependency_provider.packages() {
+            for n in dependency_provider.versions(p).unwrap() {
+                let _ = resolve(&dependency_provider, p.clone(), n.clone());
+            }
         }
     });
 }
@@ -35,11 +35,11 @@ fn bench_nested(c: &mut Criterion) {
         let data = std::fs::read_to_string(&case).unwrap();
         if name.ends_with("u16_NumberVersion.ron") {
             group.bench_function(name, |b| {
-                bench::<u16, NumberVersion>(b, &data, 0);
+                bench::<u16, NumberVersion>(b, &data);
             });
         } else if name.ends_with("str_SemanticVersion.ron") {
             group.bench_function(name, |b| {
-                bench::<&str, SemanticVersion>(b, &data, "root");
+                bench::<&str, SemanticVersion>(b, &data);
             });
         }
     }
