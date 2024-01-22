d6faa1cd7faca3d6cdae28b1d7387721360f46de
Andrew Gallant <andrew@astral.sh>
2024-01-22 09:31:49 -0500

    refactor: centralize use of hash sets/maps
    Previously, pubgrub used an alias for HashMap so that everything used
    the same implementation. But it didn't do the same for HashSet, and
    indeed, there were some uses of FxHashSet and std::collections::HashSet.
    Like for HashMap, we standard on the use of FxHashSet for everything.

--- src/type_aliases.rs
+++ src/type_aliases.rs
@@ -5,6 +5,9 @@
 /// Map implementation used by the library.
 pub type Map<K, V> = rustc_hash::FxHashMap<K, V>;
 
+/// Set implementation used by the library.
+pub type Set<V> = rustc_hash::FxHashSet<V>;
+
 /// Concrete dependencies picked by the library during [resolve](crate::solver::resolve)
 /// from [DependencyConstraints].
 pub type SelectedDependencies<P, V> = Map<P, V>;
