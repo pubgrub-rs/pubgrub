d4fb873b8c2d1630ecf39fb3943e9cac869c9dbb
konstin <konstin@mailbox.org>
2024-10-14 18:57:36 +0200

    Doc

--- src/solver.rs
+++ src/solver.rs
@@ -199,7 +199,7 @@ pub trait DependencyProvider {
     /// How this provider stores the version requirements for the packages.
     /// The requirements must be able to process the same kind of version as this dependency provider.
     ///
-    /// A common choice is [`Range`][version_range::Range].
+    /// A common choice is [`Ranges`][version_ranges::Ranges].
     type VS: VersionSet<V = Self::V>;
 
     /// Type for custom incompatibilities.
