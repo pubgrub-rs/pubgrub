62122183f81d530b4dacaf995f265bdd42547382
konstin <konstin@mailbox.org>
2025-01-30 11:17:05 +0100

    Typos

--- src/version_set.rs
+++ src/version_set.rs
@@ -68,12 +68,12 @@ pub trait VersionSet: Debug + Display + Clone + Eq {
             .complement()
     }
 
-    /// Whether the range have no overlapping segments.
+    /// Whether the ranges have no overlapping segments.
     fn is_disjoint(&self, other: &Self) -> bool {
         self.intersection(other) == Self::empty()
     }
 
-    /// Whether all range of `self` are contained in `other`.
+    /// Whether all ranges of `self` are contained in `other`.
     fn subset_of(&self, other: &Self) -> bool {
         self == &self.intersection(other)
     }
