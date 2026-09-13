19fc377ef85d5faa01dcfb9ddda71cb90fc172c4
konstin <konstin@users.noreply.github.com>
2026-09-13 22:11:02 +0000

    deploy: 0399630cccf5bb08e5621eb8efa3b71501919dda

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["pubgrub",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"pubgrub/enum.VersionParseError.html\" title=\"enum pubgrub::VersionParseError\">VersionParseError</a>",0],["impl&lt;DP: <a class=\"trait\" href=\"pubgrub/trait.DependencyProvider.html\" title=\"trait pubgrub::DependencyProvider\">DependencyProvider</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"pubgrub/enum.PubGrubError.html\" title=\"enum pubgrub::PubGrubError\">PubGrubError</a>&lt;DP&gt;<div class=\"where\">where\n    DP::<a class=\"associatedtype\" href=\"pubgrub/trait.DependencyProvider.html#associatedtype.Err\" title=\"type pubgrub::DependencyProvider::Err\">Err</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> + 'static,\n    Self: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[1321]}
