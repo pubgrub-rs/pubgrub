46242f8f0e3bcdd73936c45e2b6916a4580f9492
konstin <konstin@users.noreply.github.com>
2026-09-19 23:01:48 +0000

    deploy: 0dfd0b2744a517828ea5e28d1adcfc2e5653f16c

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["pubgrub",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"pubgrub/struct.SemanticVersion.html\" title=\"struct pubgrub::SemanticVersion\">SemanticVersion</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[279]}
---
+++ trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["pubgrub",[["impl&lt;P, VS&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"pubgrub/struct.DependencyConstraints.html\" title=\"struct pubgrub::DependencyConstraints\">DependencyConstraints</a>&lt;P, VS&gt;",0],["impl&lt;P: <a class=\"trait\" href=\"pubgrub/trait.Package.html\" title=\"trait pubgrub::Package\">Package</a>, V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.1/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"pubgrub/struct.SelectedDependencies.html\" title=\"struct pubgrub::SelectedDependencies\">SelectedDependencies</a>&lt;P, V&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[851]}
